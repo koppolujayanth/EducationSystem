08c74476e544d5d0be510f0ff70afa8a431002ee
koppolujayanth <61077156+koppolujayanth@users.noreply.github.com>
2020-12-04 11:22:30 +0530

    Add files via upload

---
+++ material.module.ts
@@ -0,0 +1,23 @@
+
+import { NgModule } from  '@angular/core';
+
+import {MatDatepickerModule} from  '@angular/material/datepicker';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {MatCardModule} from '@angular/material/card';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatListModule} from '@angular/material/list';
+import {MatRadioModule} from '@angular/material/radio';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import {MatToolbarModule} from  '@angular/material/toolbar';
+@NgModule({
+imports: [MatDatepickerModule,MatIconModule,MatButtonModule,MatCheckboxModule, MatToolbarModule,FormsModule, MatCardModule,MatFormFieldModule,MatInputModule,MatListModule,MatRadioModule,],
+ 
+exports: [FormsModule,
+MatDatepickerModule,MatIconModule,MatButtonModule,MatCheckboxModule, MatToolbarModule, MatCardModule,MatFormFieldModule,MatInputModule,MatListModule,MatRadioModule,],
+ 
+})
+ 
+export  class  MyMaterialModule { }
